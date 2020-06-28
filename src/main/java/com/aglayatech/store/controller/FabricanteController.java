6887f8a08a5533622163eb519cb61a21dd37011f
eduardo19Fu <ramieduar@gmail.com>
2020-06-27 20:01:41 -0600

    Se configuraron paginas de error y se trabajaron algunas excepciones personalizadas

--- src/main/java/com/aglayatech/store/controller/FabricanteController.java
+++ src/main/java/com/aglayatech/store/controller/FabricanteController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.aglayatech.store.model.Fabricante;
 import com.aglayatech.store.service.IFabricanteService;
 
@@ -34,7 +35,16 @@ public class FabricanteController {
 	}
 	
 	@PostMapping(value = "/save")
-	public String save(Fabricante fabricante, RedirectAttributes attributes) {
+	public String save(Fabricante fabricante, RedirectAttributes attributes, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			for(ObjectError error : result.getAllErrors()) {
+				System.out.println(error.getDefaultMessage());
+			}
+			
+			return "pages/makers/formMaker";
+		}
+		
 		serviceFabricante.guardar(fabricante);
 		attributes.addFlashAttribute("message", "Register was successfully made!");
 		return "redirect:/makers/index";
